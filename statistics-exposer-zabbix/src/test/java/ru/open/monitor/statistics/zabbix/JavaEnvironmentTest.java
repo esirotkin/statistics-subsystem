4174c119d504b82afcd496d16e266fea66343a54
Eugene Sirotkin <sirotkin@open.ru>
2019-02-27 11:00:42 +0300

    Ignored obsolete nonfunctional tests

--- statistics-exposer-zabbix/src/test/java/ru/open/monitor/statistics/zabbix/JavaEnvironmentTest.java
+++ statistics-exposer-zabbix/src/test/java/ru/open/monitor/statistics/zabbix/JavaEnvironmentTest.java
@@ -3,11 +3,13 @@ package ru.open.monitor.statistics.zabbix;
 import java.util.Map;
 import java.util.Properties;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class JavaEnvironmentTest {
 
     @Test
+    @Ignore
     public void testJavaEnvironment() throws Exception {
         Map<String, String> env = System.getenv();
         for (String envName : env.keySet()) {
@@ -16,6 +18,7 @@ public class JavaEnvironmentTest {
     }
 
     @Test
+    @Ignore
     public void testJavaProperties() throws Exception {
         Properties properties = System.getProperties();
         for (Object propertyKey : properties.keySet()) {
